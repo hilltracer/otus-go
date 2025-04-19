34d568179a28842715c35066fea8abb3d38eaec9
Denis Garsh <d.garsh@arenadata.io>
2025-04-19 11:21:40 +0300

    Simplify without doneCh

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -11,14 +11,9 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-//
-//nolint:gocognit // The function is divided logically, cognitive complexity is acceptable
-func Run(tasks []Task, n, m int) error {
-	if n <= 0 {
-		return nil
-	}
 
-	if len(tasks) == 0 {
+func Run(tasks []Task, n, m int) error {
+	if n <= 0 || len(tasks) == 0 {
 		return nil
 	}
 
@@ -26,59 +21,30 @@ func Run(tasks []Task, n, m int) error {
 	ignoreErrors := m <= 0
 
 	tasksCh := make(chan Task)
-	doneCh := make(chan struct{})
-
-	var wg sync.WaitGroup
-	var once sync.Once
-
 	var errorsCount int32
+	var wg sync.WaitGroup
 
-	startWorker := func() {
+	// Start n workers
+	for range n {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			for {
-				select {
-				case <-doneCh:
-					// If signal to stop is received, finish the processing of tasks
-					return
-				case task, ok := <-tasksCh:
-					if !ok {
-						// If tasksCh is closed, no more tasks to process
-						return
-					}
-					if err := task(); err != nil && !ignoreErrors {
-						newCount := atomic.AddInt32(&errorsCount, 1)
-						if int(newCount) >= m {
-							once.Do(func() {
-								// Add once to prevent closing the closed channel
-								close(doneCh)
-							})
-						}
-					}
+			for task := range tasksCh {
+				if err := task(); err != nil && !ignoreErrors {
+					atomic.AddInt32(&errorsCount, 1)
 				}
 			}
 		}()
 	}
 
-	// Start n workers
-	for range n {
-		startWorker()
-	}
-
 	// Send tasks to workers
-	go func() {
-		defer close(tasksCh)
-		for _, task := range tasks {
-			select {
-			case <-doneCh:
-				// If signal to stop is received, finish sending tasks
-				return
-			default:
-			}
-			tasksCh <- task
+	for _, task := range tasks {
+		if !ignoreErrors && int(atomic.LoadInt32(&errorsCount)) >= m {
+			break
 		}
-	}()
+		tasksCh <- task
+	}
+	close(tasksCh)
 
 	wg.Wait()
 
