697df809babed11e3185ae918adf6f994bfaf37c
Denis Garsh <d.garsh@arenadata.io>
2025-05-19 23:51:58 +0300

    HW5 is completed (#6)

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,32 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("m = -1 (ignore errors)", func(t *testing.T) {
+		tasksCount := 30
+		tasks := make([]Task, 0, tasksCount)
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			taskID := i
+			sleep := time.Millisecond * time.Duration(rand.Intn(100))
+			if taskID%2 == 0 {
+				tasks = append(tasks, func() error {
+					time.Sleep(sleep)
+					atomic.AddInt32(&runTasksCount, 1)
+					return fmt.Errorf("error on %d", taskID)
+				})
+			} else {
+				tasks = append(tasks, func() error {
+					time.Sleep(sleep)
+					atomic.AddInt32(&runTasksCount, 1)
+					return nil
+				})
+			}
+		}
+
+		err := Run(tasks, 4, -1)
+		require.NoError(t, err)
+		require.Equal(t, int32(tasksCount), runTasksCount)
+	})
 }
