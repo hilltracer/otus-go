c4ecea5612967d95be4da138df71d66cd42600a6
Denis Garsh <d.garsh@arenadata.io>
2025-06-15 20:11:37 +0300

    HW8 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -3,5 +3,14 @@ package main
 import "testing"
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	env := Environment{
+		"ONE":   {Value: "1"},
+		"TWO":   {NeedRemove: true},
+		"THREE": {Value: ""},
+	}
+
+	code := RunCmd([]string{"/bin/bash", "-c", `[ "$ONE" = "1" ] && [ -z "$TWO" ] && [ -z "$THREE" ]`}, env)
+	if code != 0 {
+		t.Fatalf("command returned %d, want 0", code)
+	}
 }
