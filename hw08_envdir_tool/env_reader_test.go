03553d245ca54fa3392f15cca8075513aa128a58
Denis Garsh <d.garsh@arenadata.io>
2025-06-16 15:41:52 +0300

    HW8 is completed (#9)

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,35 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	dir := t.TempDir()
+
+	mustWrite := func(name, content string) {
+		if err := os.WriteFile(dir+"/"+name, []byte(content), 0o644); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	mustWrite("FOO", "bar  \t")         // trailing ws should be trimmed
+	mustWrite("ZERO", "")               // empty file
+	mustWrite("NL", "line1\x00line2\n") // 0x00 → '\n'
+
+	env, err := ReadDir(dir)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if v := env["FOO"]; v.Value != "bar" || v.NeedRemove {
+		t.Errorf("FOO parsed wrongly: %+v", v)
+	}
+	if !env["ZERO"].NeedRemove {
+		t.Errorf("ZERO should be marked NeedRemove")
+	}
+	if v := env["NL"].Value; v != "line1\nline2" {
+		t.Errorf("NL replacement wrong: %q", v)
+	}
 }
